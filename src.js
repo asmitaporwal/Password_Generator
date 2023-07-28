fbf145fed9afaddd1b966e273728d231ddeba269
Asmita Porwal <asmita.porwal04@gmail.com>
2023-07-28 23:44:49 +0530

    function for copy password

--- src.js
+++ src.js
@@ -26,6 +26,23 @@ generateEl.addEventListener('click', () => {
 
     resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
 });
+// copy password to clipboard
+
+clipboardEl.addEventListener('click', () => {
+    const textarea = document.createElement('textarea');
+    const password = resultEl.innerText;
+
+    if (!password) {
+        return;
+    }
+
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+    alert('Password copied to clipboard');
+});
 
 //Generate password function
 
